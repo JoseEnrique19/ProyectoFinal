a75a148502531286a40730fb1eff88ce3540de31
Nereyda17 <39535058+Nereyda17@users.noreply.github.com>
2018-08-15 08:44:03 -0500

    Add files via upload
    Importacion :-)

---
+++ proyecto_web/Importar/subir.php
@@ -0,0 +1,16 @@
+<html>
+<h2>Cargar e importar archivo excel a MySQL</h2>
+
+<form action="importar.php" method="POST">    
+  
+      <input type="file" class="filestyle" data-buttonText="Seleccione archivo" name="excel">
+ 
+  
+  <br/><br/>
+    
+    <input class="btn btn-default btn-file" type='submit' name='enviar'  value="Importar"  />
+
+  
+
+    </form>
+    </html>
